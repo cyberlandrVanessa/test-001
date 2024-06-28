abc0c4319a6c4e6409509d408589ea1481c8eb93
cyberlandrVanessa <164417781+cyberlandrVanessa@users.noreply.github.com>
2024-06-28 18:58:38 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+---
+title: app 001 - sentiment analysis
+emoji: 🌍
+colorFrom: green
+colorTo: purple
+sdk: streamlit
+sdk_version: 1.35.0
+app_file: app.py
+pinned: false
+---
+Information:
+Transformers pipeline:https://towardsdatascience.com/transformers-pipeline-a-comprehensive-guide-for-nlp-tasks-e4ae260a13db
+
+README FILE HERE > https://docs.google.com/document/d/1vfzPba5eqSnfXkRSk6Z-mWvx67zz67ghw9XwfZ3R41E/edit?usp=sharing
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,50 @@
+from transformers import pipeline
+import streamlit as st
+from streamlit.components.v1 import html
+import json
+
+# Initialize pipeline globally to avoid reloading it every time
+pipe = pipeline('sentiment-analysis')
+
+# Functions
+def open_page(url):
+    open_script = f"""
+        <script type="text/javascript">
+            window.open('{url}', '_blank').focus();
+        </script>
+    """
+    html(open_script)
+
+def analyseReview(textIn):   
+    if textIn:
+        out = pipe(textIn)
+        first_element = out[0]
+        label = first_element["label"]
+        score = first_element["score"] * 100  # Convert to percentage
+        #st.header(f"There is a {score:.2f}% probability that you have {label} feelings about the movie.")
+        html_str = f"""
+        <style>
+        p.a {{
+          color: white;
+          background-color: black;
+          font: 15px Arial;
+        }}
+        </style>
+        <p class="a"><br>&nbsp;&nbsp There is a {score:.2f}% probability that you have {label} feelings about the movie.&nbsp;&nbsp&nbsp<br>&nbsp</p>
+        """
+        st.markdown(html_str, unsafe_allow_html=True)
+        st.json(out)
+ 
+def main():
+    st.write("_______________________________________")
+    st.button('View README file', on_click=open_page, args=('https://docs.google.com/document/d/1vfzPba5eqSnfXkRSk6Z-mWvx67zz67ghw9XwfZ3R41E/edit?usp=sharing',))
+    st.title("App 01: Sentiment Analysis TTT")
+    st.header("HuggingFace, Json, Python, Streamlit, Transformers")
+    text = st.text_area('Write a review of the last movie you saw then press Ctrl + Enter.')
+    
+    if st.button('My Review'):
+        analyseReview(text)
+
+# Logic
+if __name__ == '__main__':
+    main()
